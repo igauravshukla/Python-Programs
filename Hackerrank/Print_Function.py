33e816cc6c57c239205af8e4d32e6607ad69512f
igauravshukla <59728628+igauravshukla@users.noreply.github.com>
2020-02-18 00:32:31 +0530

    Example of using print function

---
+++ Hackerrank/Print_Function.py
@@ -0,0 +1,26 @@
+'''
+Read an integer N.
+
+Without using any string methods, try to print the following:
+123...N
+Note that "..." represents the values in between.
+
+Input Format :
+The first line contains an integer N.
+
+Output Format :
+Output the answer as explained in the task.
+
+Sample Input 0
+3
+
+Sample Output 0
+123
+
+'''
+
+n = int(input())
+i=1
+while(i<=n):
+    print(i,end='')
+    i+=1
